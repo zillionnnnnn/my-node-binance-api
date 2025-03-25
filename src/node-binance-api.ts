74458d27e1b4b953bf996eb0a23cb5f8c553f2f3
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-03-25 18:02:36 +0000

    update ts-ignore

--- src/node-binance-api.ts
+++ src/node-binance-api.ts
@@ -6,6 +6,7 @@ import crypto from 'crypto';
 import file from 'fs';
 import url from 'url';
 import JSONbig from 'json-bigint';
+// @ts-ignore
 import { HttpsProxyAgent } from 'https-proxy-agent';
 // @ts-ignore
 import { SocksProxyAgent } from 'socks-proxy-agent';
